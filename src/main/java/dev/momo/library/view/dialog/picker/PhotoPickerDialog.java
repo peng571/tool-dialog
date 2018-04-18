279ee10b0e99252c24ab18e0dac54b127a137bdc
Momo Peng <yr.peng571@gmail.com>
2018-04-18 13:09:36 +0800

    update pickup dialog

--- src/main/java/dev/momo/library/view/dialog/picker/PhotoPickerDialog.java
+++ src/main/java/dev/momo/library/view/dialog/picker/PhotoPickerDialog.java
@@ -2,6 +2,7 @@ package dev.momo.library.view.dialog.picker;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 
 import dev.momo.library.core.image.PhotoPicker;
 import dev.momo.library.view.R;
@@ -20,19 +21,19 @@ public class PhotoPickerDialog {
 
     private final static int REQUEST_PICK_PHOTO_CODE = 1;
 
-    public static PickerDialog get(final Activity activity) {
+    public static PickerDialog get(final Activity activity, @Nullable String authorities) {
         return PickerDialog.newInstance(R.string.photo_title, REQUEST_PICK_PHOTO_CODE)
                 .addItem(R.string.photo_camera, (int index, String name) ->
-                        PhotoPicker.startTakePictureIntent(activity))
+                        PhotoPicker.startTakePictureIntent(activity, authorities))
                 .addItem(R.string.photo_from_gallery, (int index, String name) ->
                         PhotoPicker.startPickImageIntent(activity));
     }
 
 
-    public static PickerDialog get(final Fragment fragment) {
+    public static PickerDialog get(final Fragment fragment, @Nullable String authorities) {
         return PickerDialog.newInstance(R.string.photo_title, REQUEST_PICK_PHOTO_CODE)
                 .addItem(R.string.photo_camera, (int index, String name) ->
-                        PhotoPicker.startTakePictureIntent(fragment))
+                        PhotoPicker.startTakePictureIntent(fragment, authorities))
                 .addItem(R.string.photo_from_gallery, (int index, String name) ->
                         PhotoPicker.startPickImageIntent(fragment));
     }
